5ae8bd170296a1a22594c9e64fc53f83f5ae8106
mhamedbelhachemi <mhamed79@live.fr>
2020-03-11 23:26:29 +0100

    validation des test mais aucun n'est correct (evaluation.py) les tests appel.l et si.l ne marchent pas pour ts

--- src/Compiler.java
+++ src/Compiler.java
@@ -41,11 +41,11 @@ public class Compiler
 	    SaNode saRoot = sc2sa.getRoot();
 	    new Sa2Xml(saRoot, baseName);
 		    
-	    /*System.out.println("[TABLE SYMBOLES]");
+	    System.out.println("[TABLE SYMBOLES]");
 	    Ts table = new Sa2ts(saRoot).getTableGlobale();
 	    table.afficheTout(baseName);
 
-	    System.out.println("[C3A]");
+	    /*System.out.println("[C3A]");
 	    C3a c3a = new Sa2c3a(saRoot, table).getC3a();
 	    c3a.affiche(baseName);
 
--- src/Sa2ts.java
+++ src/Sa2ts.java
@@ -6,17 +6,17 @@ import ts.TsItemVar;
 public class Sa2ts extends SaDepthFirstVisitor{
     private Ts Global_Table ;
     private Ts Local_Tables;
-    private boolean isGlobal;
-    private boolean isParam;
-    private boolean isVariable;
+    public static final String GLOBAL = "GLOBAL";
+    public static final String PARAMETRE = "PARAMETRE";
+    public static final String VARIABLE = "VARIABLE";
+    public String State;
+
     private int indentation = 0;
 
     public Sa2ts(SaNode root){
         Global_Table = new Ts();
+        State = GLOBAL;
         root.accept(this);
-        isGlobal = true;
-        isParam = false;
-        isVariable = false;
     }
 
     public void defaultIn(SaNode node)
@@ -40,25 +40,22 @@ public class Sa2ts extends SaDepthFirstVisitor{
     public Void visit(SaDecVar node)
     {
         defaultIn(node);
-        if(isGlobal) {
-            if (Global_Table.variables.containsKey(node.getNom()))
+        if(State == GLOBAL) {
+            if (varExists(node.getNom(), Global_Table))
                 throw new RuntimeException("cette variable a deja été déclaré");
             node.tsItem = Global_Table.addVar(node.getNom(), 1);
         }
-        isParam = true;
-        if(isParam) {
-            if (Local_Tables.variables.containsKey(node.getNom()))
+        if(State == PARAMETRE) {
+            if (varExists(node.getNom(), Local_Tables))
+                throw new RuntimeException("cette variable a deja été déclaré");
+            node.tsItem = Local_Tables.addParam(node.getNom());
+        }
+        if(State == VARIABLE) {
+            if (varExists(node.getNom(), Global_Table))
                 throw new RuntimeException("cette variable a deja été déclaré");
             node.tsItem = Local_Tables.addVar(node.getNom(), 1);
         }
-        isVariable = true;
-        if(isVariable) {
-            if (Local_Tables.variables.containsKey(node.getNom()))
-                throw new RuntimeException("cette variable a deja été déclaré");
-            node.tsItem = Local_Tables.addVar(node.getNom(), 1);
-        }
-        isParam = false;
-        isVariable = false;
+        State = GLOBAL;
         defaultOut(node);
         return null;
     }
@@ -67,7 +64,7 @@ public class Sa2ts extends SaDepthFirstVisitor{
         defaultIn(node);
         String nom = node.getNom();
         int taille = node.getTaille();
-        if(Global_Table.variables.containsKey(nom)){ throw new RuntimeException("Cette variable existe deja");}
+        if(varExists(nom, Global_Table)){ throw new RuntimeException("Cette variable existe deja");}
         else node.tsItem = Global_Table.addVar(nom,taille);
         defaultOut(node);
         return null;
@@ -78,13 +75,14 @@ public class Sa2ts extends SaDepthFirstVisitor{
         Local_Tables = new Ts();
         String nom = node.getNom();
         int nbr_args;
-        if(node.getParametres() != null) nbr_args = 0;
+        if(node.getParametres() == null) nbr_args = 0;
         else nbr_args = node.getParametres().length();
         if (node.getParametres() != null) node.getParametres().accept(this);
         if (node.getVariable() != null) node.getVariable().accept(this);
         if (node.getCorps() != null) node.getCorps().accept(this);
         Local_Tables = Global_Table.getTableLocale(node.getNom());
         node.tsItem = Global_Table.addFct(nom,nbr_args,Local_Tables,node);
+        State = GLOBAL;
         defaultOut(node);
         return null;
     }
@@ -92,15 +90,30 @@ public class Sa2ts extends SaDepthFirstVisitor{
     public Void visit(SaVarSimple node)
     {
         defaultIn(node);
+        String nom = node.getNom();
+        if(!varExists(nom, Local_Tables) && !varExists(nom, Global_Table)){
+            throw new RuntimeException("cette variable n'as pas été déclaré");
+        }
+        TsItemVar tsItemVar;
+        if (State == GLOBAL) {
+            tsItemVar = Global_Table.getVar(node.getNom());
+        } else {
+            tsItemVar = Local_Tables.getVar(node.getNom());
+        }
 
+        node.tsItem = tsItemVar;
         defaultOut(node);
         return null;
     }
 
+
     public Void visit(SaVarIndicee node)
     {
         defaultIn(node);
-        node.getIndice().accept(this);
+        String nom = node.getNom();
+        if(varExists(nom, Local_Tables)) throw new RuntimeException(nom + "est une variable local");
+        if(varExists(nom, Global_Table)) throw new RuntimeException(nom + "n'as pas été déclaré");
+        node.tsItem = Global_Table.getVar(nom);
         defaultOut(node);
         return null;
     }
@@ -108,11 +121,25 @@ public class Sa2ts extends SaDepthFirstVisitor{
     public Void visit(SaAppel node)
     {
         defaultIn(node);
-        if(node.getArguments() != null) node.getArguments().accept(this);
+        String nom = node.getNom();
+        int nbr_args;
+        if(node.getArguments() != null) nbr_args = 0;
+        else nbr_args = node.getArguments().length();
+        TsItemFct tsItemFct = Global_Table.getFct(nom);
+        if(!FncExists(nom)) throw new RuntimeException("cette fonction n'existe pas");
+        if(nbr_args != tsItemFct.nbArgs) throw new RuntimeException("le nombre d'argument n'est pas celui attendu");
+        node.tsItem = tsItemFct;
         defaultOut(node);
         return null;
     }
 
+    private boolean varExists(String nom, Ts table) {
+        return table.getVar(nom) != null;
+    }
+
+    private boolean FncExists(String nom) {
+        return Global_Table.getFct(nom) != null;
+    }
 
     public Ts getTableGlobale() {
         return Global_Table;
