3e00df2aca190d94234f53f731fc07608663789c
mhamedbelhachemi <mhamed79@live.fr>
2020-03-11 17:24:36 +0100

    implementation du visiteur de sadecfonc et sadectab

--- src/Sa2ts.java
+++ src/Sa2ts.java
@@ -7,101 +7,31 @@ public class Sa2ts extends SaDepthFirstVisitor{
     private Ts Global_Table ;
     private Ts Local_Tables;
     private boolean isParam;
+    private int indentation = 0;
 
     public Sa2ts(SaNode root){
         Global_Table = new Ts();
         root.accept(this);
     }
 
-    public Ts getTableGlobale() {
-        return Global_Table;
-    }
-
     public void defaultIn(SaNode node)
     {
+        for(int i = 0 ; i < indentation ; i++){
+            System.out.print(" ");
+        }
+        System.out.println("<"+node.getClass().getSimpleName()+">");
+        indentation++;
     }
 
     public void defaultOut(SaNode node)
     {
+        indentation--;
+        for(int i = 0 ; i < indentation ; i++){
+            System.out.print(" ");
+        }
+        System.out.println("</"+node.getClass().getSimpleName()+">");
     }
 
-    public Void visit(SaProg node)
-    {
-        defaultIn(node);
-        if(node.getVariables() != null)
-            node.getVariables().accept(this);
-        if(node.getFonctions() != null)
-            node.getFonctions().accept(this);
-        defaultOut(node);
-        return null;
-    }
-
-    // DEC -> var id taille
-    public Void visit(SaDecTab node){
-        defaultIn(node);
-        defaultOut(node);
-        return null;
-    }
-
-    public Void visit(SaExp node)
-    {
-        defaultIn(node);
-        defaultOut(node);
-        return null;
-    }
-
-    // EXP -> entier
-    public Void visit(SaExpInt node)
-    {
-        defaultIn(node);
-        defaultOut(node);
-        return null;
-    }
-    public Void visit(SaExpVar node)
-    {
-        defaultIn(node);
-        node.getVar().accept(this);
-        defaultOut(node);
-        return null;
-    }
-
-    public Void visit(SaInstEcriture node)
-    {
-        defaultIn(node);
-        node.getArg().accept(this);
-        defaultOut(node);
-        return null;
-    }
-
-    public Void visit(SaInstTantQue node)
-    {
-        defaultIn(node);
-        node.getTest().accept(this);
-        node.getFaire().accept(this);
-        defaultOut(node);
-        return null;
-    }
-    public Void visit(SaLInst node)
-    {
-        defaultIn(node);
-        node.getTete().accept(this);
-        if(node.getQueue() != null) node.getQueue().accept(this);
-        defaultOut(node);
-        return null;
-    }
-
-    // DEC -> fct id LDEC LDEC LINST
-    public Void visit(SaDecFonc node)
-    {
-        defaultIn(node);
-        if(node.getParametres() != null) node.getParametres().accept(this);
-        if(node.getVariable() != null) node.getVariable().accept(this);
-        node.getCorps().accept(this);
-        defaultOut(node);
-        return null;
-    }
-
-    // DEC -> var id
     public Void visit(SaDecVar node)
     {
         defaultIn(node);
@@ -109,22 +39,28 @@ public class Sa2ts extends SaDepthFirstVisitor{
         return null;
     }
 
-    public Void visit(SaInstAffect node)
-    {
+    public Void visit(SaDecTab node){
         defaultIn(node);
-        node.getLhs().accept(this);
-        node.getRhs().accept(this);
+        String nom = node.getNom();
+        int taille = node.getTaille();
+        if(Global_Table.variables.containsKey(nom)){ throw new RuntimeException("Cette variable existe deja");}
+        node.tsItem = Global_Table.addVar(nom,taille);
         defaultOut(node);
         return null;
     }
 
-    // LDEC -> DEC LDEC
-    // LDEC -> null
-    public Void visit(SaLDec node)
-    {
+    public Void visit(SaDecFonc node){
         defaultIn(node);
-        node.getTete().accept(this);
-        if(node.getQueue() != null) node.getQueue().accept(this);
+        Local_Tables = new Ts();
+        String nom = node.getNom();
+        int nbr_args;
+        if(node.getParametres() != null) nbr_args = 0;
+        else nbr_args = node.getParametres().length();
+        if (node.getParametres() != null) node.getParametres().accept(this);
+        if (node.getVariable() != null) node.getVariable().accept(this);
+        if (node.getCorps() != null) node.getCorps().accept(this);
+        Local_Tables = Global_Table.getTableLocale(node.getNom());
+        node.tsItem = Global_Table.addFct(nom,nbr_args,Local_Tables,node);
         defaultOut(node);
         return null;
     }
@@ -132,6 +68,15 @@ public class Sa2ts extends SaDepthFirstVisitor{
     public Void visit(SaVarSimple node)
     {
         defaultIn(node);
+        
+        defaultOut(node);
+        return null;
+    }
+
+    public Void visit(SaVarIndicee node)
+    {
+        defaultIn(node);
+        node.getIndice().accept(this);
         defaultOut(node);
         return null;
     }
@@ -144,154 +89,8 @@ public class Sa2ts extends SaDepthFirstVisitor{
         return null;
     }
 
-    public Void visit(SaExpAppel node)
-    {
-        defaultIn(node);
-        node.getVal().accept(this);
-        defaultOut(node);
-        return null;
-    }
 
-    // EXP -> add EXP EXP
-    public Void visit(SaExpAdd node)
-    {
-        defaultIn(node);
-        node.getOp1().accept(this);
-        node.getOp2().accept(this);
-        defaultOut(node);
-        return null;
-    }
-
-    // EXP -> sub EXP EXP
-    public Void visit(SaExpSub node)
-    {
-        defaultIn(node);
-        node.getOp1().accept(this);
-        node.getOp2().accept(this);
-        defaultOut(node);
-        return null;
-    }
-
-    // EXP -> mult EXP EXP
-    public Void visit(SaExpMult node)
-    {
-        defaultIn(node);
-        node.getOp1().accept(this);
-        node.getOp2().accept(this);
-        defaultOut(node);
-        return null;
-    }
-
-    // EXP -> div EXP EXP
-    public Void visit(SaExpDiv node)
-    {
-        defaultIn(node);
-        node.getOp1().accept(this);
-        node.getOp2().accept(this);
-        defaultOut(node);
-        return null;
-    }
-
-    // EXP -> inf EXP EXP
-    public Void visit(SaExpInf node)
-    {
-        defaultIn(node);
-        node.getOp1().accept(this);
-        node.getOp2().accept(this);
-        defaultOut(node);
-        return null;
-    }
-
-    // EXP -> eq EXP EXP
-    public Void visit(SaExpEqual node)
-    {
-        defaultIn(node);
-        node.getOp1().accept(this);
-        node.getOp2().accept(this);
-        defaultOut(node);
-        return null;
-    }
-
-    // EXP -> and EXP EXP
-    public Void visit(SaExpAnd node)
-    {
-        defaultIn(node);
-        node.getOp1().accept(this);
-        node.getOp2().accept(this);
-        defaultOut(node);
-        return null;
-    }
-
-
-    // EXP -> or EXP EXP
-    public Void visit(SaExpOr node)
-    {
-        defaultIn(node);
-        node.getOp1().accept(this);
-        node.getOp2().accept(this);
-        defaultOut(node);
-        return null;
-    }
-
-    // EXP -> not EXP
-    public Void visit(SaExpNot node)
-    {
-        defaultIn(node);
-        node.getOp1().accept(this);
-        defaultOut(node);
-        return null;
-    }
-
-
-    public Void visit(SaExpLire node)
-    {
-        defaultIn(node);
-        defaultOut(node);
-        return null;
-    }
-
-    public Void visit(SaInstBloc node)
-    {
-        defaultIn(node);
-        node.getVal().accept(this);
-        defaultOut(node);
-        return null;
-    }
-
-    public Void visit(SaInstSi node)
-    {
-        defaultIn(node);
-        node.getTest().accept(this);
-        node.getAlors().accept(this);
-        if(node.getSinon() != null) node.getSinon().accept(this);
-        defaultOut(node);
-        return null;
-    }
-
-    // INST -> ret EXP
-    public Void visit(SaInstRetour node)
-    {
-        defaultIn(node);
-        node.getVal().accept(this);
-        defaultOut(node);
-        return null;
-    }
-
-
-    public Void visit(SaLExp node)
-    {
-        defaultIn(node);
-        node.getTete().accept(this);
-        if(node.getQueue() != null)
-            node.getQueue().accept(this);
-        defaultOut(node);
-        return null;
-    }
-    public Void visit(SaVarIndicee node)
-    {
-        defaultIn(node);
-        node.getIndice().accept(this);
-        defaultOut(node);
-        return null;
+    public Ts getTableGlobale() {
+        return Global_Table;
     }
 }
